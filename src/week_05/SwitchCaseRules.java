7bffbcde999c628fb7df84d28fc29195bf0abd94
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-03-19 16:00:50 +0300

    Initial commit

---
+++ src/week_05/SwitchCaseRules.java
@@ -0,0 +1,21 @@
+package week_05;
+
+public class SwitchCaseRules {
+    public static void main(String[] args) {
+        //byte data=1;
+        //short date =1;
+        //int data=0;
+        //long data=1;      //we can't
+        //float data=1;     //we can't
+        //double data=1;    //we can't
+        //boolean data=true;    //we can't
+        char data ='1';
+
+        switch (data){
+
+
+
+        }
+
+    }
+}
