a166f1efc8df0a9d5bf706c815e33fcb44be719c
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-03-19 20:47:28 +0300

    week 5 String second task

---
+++ src/week_05/RedAndBlue.java
@@ -0,0 +1,26 @@
+package week_05;
+
+public class RedAndBlue {
+    /*
+    Given a string, if the string begins with "red" or "blue" print that color string, otherwise
+    print the empty string.
+
+                    input : "redxx"-> "red"
+                    input : "xxred"-> ""
+                    input : "blueTimes"-> "blue"
+     */
+    public static void main(String[] args) {
+
+        String word = "redxx";
+
+        if(word.indexOf("red")==0){//startsWith()
+            System.out.println("red");
+        } else if (word.indexOf("blue")==0) {
+            System.out.println("blue");
+        }else{
+            System.out.println("\"\"");
+        }
+
+
+    }
+}
