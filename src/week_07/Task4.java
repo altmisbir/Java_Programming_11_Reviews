0d61e816d4f19ec00e286f92cd32e98cb2f817fd
Recep ÇAVUŞOĞLU <91440830+altmisbir@users.noreply.github.com>
2023-04-10 23:00:46 +0300

    week 7 task 4 ready

---
+++ src/week_07/Task4.java
@@ -0,0 +1,17 @@
+package week_07;
+
+public class Task4 {
+     /*
+    Task 4 :  For Each - Nested For
+            Longest Palindrome
+             Given a String array. Find the longest Palindrome String in your array.
+                    Ex:
+                    Input: ["java", "longer word", "civic", "apple", "racecar", "mom", "anna"] Output: racecar
+                    Ex:
+                    Input: ["abc", "dna", "kevin", "joe", "lamp"]
+                    Output: No palindrome
+     */
+     public static void main(String[] args) {
+
+     }
+}
