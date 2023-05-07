7cfb5c410f08fbc723d59b3e27be0d0b76e8d38b
altmisbir <altmisbir@msn.com>
2023-05-07 15:41:49 +0300

    Week 12 : Encapsulation

--- src/week_12/Person.java
+++ src/week_12/Person.java
@@ -4,49 +4,45 @@ public class Person {
     private String name;
     private int age;
 
-    public Person() {
-    }
-
-    public Person(String name) {
-        setName(name);
-    }
-
-    public Person(int age) {
-        setAge(age);
-    }
-
-    public Person(String name, int age) {
-        setName(name);
-        setAge(age);
-    }
 
     public void setName(String name) {
-        boolean isValidName = true;
-        for (char c :name.toCharArray()){
-            if (Character.isLetter(c)) {
-                isValidName = false;
+        boolean isValidName=true;
+        for (char c : name.toCharArray()) {
+            if (!Character.isLetter(c)){
+                isValidName=false;
                 break;
             }
         }
-        if (isValidName) {
+        if (isValidName){
             this.name = name;
         }else {
             System.out.println("Your name is not valid");
             System.exit(0);
+            //return
         }
 
     }
 
     public void setAge(int age) {
 
-        if (age < 0) {
-            System.out.println("You can not assign negative numbers in age ");
+        if (age<0){
+            System.out.println("you can not assign negative numbers in age");
             System.exit(0);
         }else {
             this.age = age;
         }
+
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
     public String toString() {
         return "Person{" +
                 "name='" + name + '\'' +
--- src/week_12/TestPerson.java
+++ src/week_12/TestPerson.java
@@ -3,12 +3,15 @@ package src.week_12;
 public class TestPerson {
     public static void main(String[] args) {
         Person person = new Person();
+        // person.name="12234"; // we cut the connection between variable in order to control it
         person.setName("Adam");
+        //  person.age=-12; // we cut the connection between variable in order to control it
         person.setAge(34);
-
         System.out.println(person);
-
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
     }
-
-
 }
+
+
+
