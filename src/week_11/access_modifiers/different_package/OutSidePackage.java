7639eba943c8d3b008a092ea503a4af2de82ba88
altmisbir <altmisbir@msn.com>
2023-06-15 00:40:02 +0300

    week 11 : static done

---
+++ src/week_11/access_modifiers/different_package/OutSidePackage.java
@@ -0,0 +1,15 @@
+package src.week_11.access_modifiers.different_package;
+
+import src.week_11.access_modifiers.same_package.Student;
+
+public class OutSidePackage {
+
+    public static void main(String[] args) {
+        Student student=new Student();
+        System.out.println(student.publicField);
+        // System.out.println(student.defaultField);
+        // System.out.println(student.privateField);
+        // System.out.println(student.protectedField);
+    }
+
+}
