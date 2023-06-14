3bf107847ce26918bd5b34782d5ec3887149711e
altmisbir <altmisbir@msn.com>
2023-06-14 23:27:44 +0300

    week 11 : static done

---
+++ src/week_11/statics/BrowserUtilImpl.java
@@ -0,0 +1,17 @@
+package src.week_11.statics;
+
+import static src.week_11.statics.BrowserUtils.*;
+
+
+public class BrowserUtilImpl {
+
+    public static void main(String[] args) {
+        //  BrowserUtils.open();
+        //  BrowserUtils.close();
+        //  BrowserUtils.refresh();
+        open();
+        refresh();
+        close();
+
+    }
+}
---
+++ src/week_11/statics/Cake.java
@@ -0,0 +1,10 @@
+package src.week_11.statics;
+
+public class Cake {
+
+    public static int numberOfSlices=10;
+
+    public void eat(int slice){
+        numberOfSlices-=slice;
+    }
+}
