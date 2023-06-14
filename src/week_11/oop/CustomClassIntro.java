830d3b0aaf17b195966e249ec3d7be6ffe24edda
altmisbir <altmisbir@msn.com>
2023-06-14 23:08:17 +0300

    week 11 : OOP started

---
+++ src/week_11/oop/CustomClassIntro.java
@@ -0,0 +1,15 @@
+package src.week_11.oop;
+
+public class CustomClassIntro {
+    public static void main(String[] args) {
+
+        String studentName="Rinor";
+        String studentName2="Burak";
+
+        Student student=new Student("Burak","Can",18);
+        System.out.println(student);
+
+    }
+
+}
+
---
+++ src/week_11/oop/Student.java
@@ -0,0 +1,28 @@
+package src.week_11.oop;
+
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int age;
+    //static  public String course="Cydeo";
+    static  public String course;
+    static {
+        course="Cydeo";
+    }
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
