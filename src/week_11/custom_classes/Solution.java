a028546495f2cb3426ee99b4be6b17123532cf24
altmisbir <altmisbir@msn.com>
2023-06-14 23:23:02 +0300

    week 11 : send multiple class in one file

---
+++ src/week_11/custom_classes/Solution.java
@@ -0,0 +1,78 @@
+package src.week_11.custom_classes;
+
+import java.util.Scanner;
+
+class  Teacher{
+    public  String name;
+    public int age;
+    public char gender;
+
+    public Teacher(String name, int age, char gender) {
+        // this.name = name;
+        // this.age = age;
+
+        this(name, age);
+        this.gender = gender;
+
+    }
+
+    public Teacher(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Teacher() {
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+}
+
+class BMW{
+
+}
+
+class Mercedes{
+
+}
+class Audi{
+
+}
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+        src.week_11.custom_classes.constructors.Teacher teacher1=new src.week_11.custom_classes.constructors.Teacher("Adam",34,'M');
+        src.week_11.custom_classes.constructors.Teacher teacher2=new  src.week_11.custom_classes.constructors.Teacher("Aysun",25);
+
+        System.out.println(teacher2);
+
+        teacher2.gender='F';
+        System.out.println(teacher2);
+
+
+
+
+        src.week_11.custom_classes.constructors.Teacher teacher3=new  src.week_11.custom_classes.constructors.Teacher();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter teacher name");
+
+        teacher3.name=scanner.nextLine();
+        System.out.println("Enter teacher age");
+        teacher3.age= scanner.nextInt();
+        System.out.println("Enter gender");
+
+        teacher3.gender=scanner.next().charAt(0);
+
+        System.out.println(teacher3);
+
+    }
+}
